4531be8e795b880fbdadceb2ae23c9cf6afbcb1d
sajeeldev <sajeelwebdevagmail.com>
2024-09-03 23:50:43 +0500

    main

--- Arithmetic Operators test/Arithmetic Operators.php
+++ Arithmetic Operators test/Arithmetic Operators.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Arithmetic Operators</title>
+    <title>Arithmetic Operators - Test</title>
 </head>
 <body>
-    <h1>Arithmetic Operators</h1>
+    <h1>Arithmetic Operators - Test</h1>
     <?php
 
         // Addition
